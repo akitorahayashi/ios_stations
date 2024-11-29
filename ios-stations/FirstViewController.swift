1ac704bb7c4c3d1cd3d6ab5b97f628bfb1aa81b5
akitora <hayashi20030703@gmail.com>
2024-11-29 12:07:45 +0900

    Recommit

--- ios-stations/FirstViewController.swift
+++ ios-stations/FirstViewController.swift
@@ -10,6 +10,7 @@ class FirstViewController: UIViewController {
     var books: [Book]?
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.Theme.main
     }
 }
 

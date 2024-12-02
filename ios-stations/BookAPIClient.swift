530263a56d273dc936fbc691ebcbd5003fc9ad88
akitora <hayashi20030703@gmail.com>
2024-12-02 12:33:08 +0900

    UITableViewを作成した

--- ios-stations/BookAPIClient.swift
+++ ios-stations/BookAPIClient.swift
@@ -3,12 +3,28 @@
 //  ios-stations
 //
 
+import Alamofire
+
 protocol BookAPIClientProtocol {
     func fetchBooks(offset: Int, completion: @escaping ([Book]?) -> Void)
 }
 
 class BookAPIClient: BookAPIClientProtocol {
+    static let shared = BookAPIClient()
+    // APIのエンドポイント
+    private let baseURL = "https://railway.bookreview.techtrain.dev/public/books"
+    
+    
     func fetchBooks(offset: Int, completion: @escaping ([Book]?) -> Void) {
-        completion(nil)
+//        AF.request(baseURL, method: .get)
+//                    .validate() // ステータスコードの検証
+//                    .responseDecodable(of: [Book].self) { response in
+//                        switch response.result {
+//                        case .success(let books):
+//                            completion(books)
+//                        case .failure(let error):
+//                            completion(nil)
+//                        }
+//                    }
     }
 }

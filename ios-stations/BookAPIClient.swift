10db73a2aecc11bc2893b8237359087746146af9
akitora <hayashi20030703@gmail.com>
2024-12-02 13:30:12 +0900

    データ通信の機能を実装した

--- ios-stations/BookAPIClient.swift
+++ ios-stations/BookAPIClient.swift
@@ -16,15 +16,15 @@ class BookAPIClient: BookAPIClientProtocol {
     
     
     func fetchBooks(offset: Int, completion: @escaping ([Book]?) -> Void) {
-//        AF.request(baseURL, method: .get)
-//                    .validate() // ステータスコードの検証
-//                    .responseDecodable(of: [Book].self) { response in
-//                        switch response.result {
-//                        case .success(let books):
-//                            completion(books)
-//                        case .failure(let error):
-//                            completion(nil)
-//                        }
-//                    }
+        AF.request(baseURL, method: .get)
+                    .validate()
+                    .responseDecodable(of: [Book].self) { response in
+                        switch response.result {
+                        case .success(let books):
+                            completion(books)
+                        case .failure(let error):
+                            completion(nil)
+                        }
+                    }
     }
 }
--- ios-stations/FirstViewController.swift
+++ ios-stations/FirstViewController.swift
@@ -8,26 +8,51 @@ import UIKit
 class FirstViewController: UIViewController {
     
     var books: [Book]?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "BookCell")
+        
+        fetchBooks()
     }
-    @IBOutlet weak var clickButton: UIButton!
     
-    @IBAction func presentSecondViewController(_ sender: UIButton) {
-        let secondViewController = SecondViewController(url: "https://techbowl.co.jp/")
-
-        // presentメソッドでSecondViewControllerを表示
-        present(secondViewController, animated: true, completion: nil)
+    func fetchBooks() {
+        BookAPIClient.shared.fetchBooks(offset: 0) { [weak self] (fetchedBooks) in
+            guard let self = self else { return }
+            
+            if let fetchedBooks = fetchedBooks {
+                self.books = fetchedBooks
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } else {
+                print("Failed to fetch books")
+            }
+        }
     }
 }
 
 extension FirstViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books?.count ?? 0
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
+        
+        if let book = books?[indexPath.row] {
+//            cell.textLabel?.text = book.title
+//            cell.detailTextLabel?.text = book.author
+        }
+        
+        return cell
     }
 }
 
